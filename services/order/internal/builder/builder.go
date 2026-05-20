c25bc9ba217ba36b78b82fecb73ee86fd811b7bf
Sergei Skoropupov <donckou@gmail.com>
2026-05-20 08:12:23 +0300

    feat: заглушки для router

--- services/order/internal/builder/builder.go
+++ services/order/internal/builder/builder.go
@@ -1,7 +1,11 @@
 package builder
 
-import orders "github.com/BeInBloom/big-deal/generated/go/order-service/openapi/v1"
+import (
+	orders "github.com/BeInBloom/big-deal/generated/go/order-service/openapi/v1"
+	"github.com/BeInBloom/big-deal/services/order/internal/handlers"
+)
 
 func Build() (*orders.Server, error) {
-	panic("not implemented")
+	h := handlers.New()
+	return orders.NewServer(h)
 }
---
+++ services/order/internal/handlers/handlers.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"context"
+
+	orders "github.com/BeInBloom/big-deal/generated/go/order-service/openapi/v1"
+	"github.com/BeInBloom/big-deal/services/order/internal/models"
+)
+
+type orderService interface {
+	CancelOrder(context.Context, models.OrderId) error
+	CreateOrder(context.Context, models.UserId, []models.PartId) (models.Order, error)
+	GetOrder(context.Context, models.OrderId) (models.Order, error)
+	PayOrder(context.Context, models.OrderId, models.PaymentMethod) error
+}
+
+type Handlers struct {
+	orderService orderService
+}
+
+func New() *Handlers {
+	return &Handlers{}
+}
+
+func (h *Handlers) OrderServiceCancelOrder(
+	ctx context.Context,
+	params orders.OrderServiceCancelOrderParams,
+) (orders.OrderServiceCancelOrderRes, error) {
+	panic("implement me")
+}
+
+func (h *Handlers) OrderServiceCreateOrder(
+	ctx context.Context,
+	req *orders.CreateOrderRequest,
+) (orders.OrderServiceCreateOrderRes, error) {
+	panic("implement me")
+}
+
+func (h *Handlers) OrderServiceGetOrder(
+	ctx context.Context,
+	params orders.OrderServiceGetOrderParams,
+) (orders.OrderServiceGetOrderRes, error) {
+	panic("implement me")
+}
+
+func (h *Handlers) OrderServicePayOrder(
+	ctx context.Context,
+	req *orders.PayOrderRequest,
+	params orders.OrderServicePayOrderParams,
+) (orders.OrderServicePayOrderRes, error) {
+	panic("implement me")
+}
