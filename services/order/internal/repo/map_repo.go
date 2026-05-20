1a542789385cc715d926e7252bd066d83031c9c3
Sergei Skoropupov <donckou@gmail.com>
2026-05-21 00:15:51 +0300

    feat: добавлен map repo для заказов

---
+++ services/order/internal/repo/map_repo.go
@@ -0,0 +1,44 @@
+package repo
+
+import (
+	"context"
+	"sync"
+
+	"github.com/BeInBloom/big-deal/services/order/internal/models"
+	"github.com/BeInBloom/big-deal/services/order/internal/services"
+)
+
+type MapRepo struct {
+	mu     sync.RWMutex
+	orders map[models.OrderId]models.OrderSnapshot
+}
+
+func NewMapRepo() *MapRepo {
+	return &MapRepo{
+		orders: make(map[models.OrderId]models.OrderSnapshot),
+	}
+}
+
+func (r *MapRepo) Get(_ context.Context, id models.OrderId) (models.OrderSnapshot, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	order, ok := r.orders[id]
+	if !ok {
+		return models.OrderSnapshot{}, services.ErrOrderNotFound
+	}
+
+	return cloneOrderSnapshot(order), nil
+}
+
+func (r *MapRepo) Save(_ context.Context, order models.OrderSnapshot) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.orders[order.Id] = cloneOrderSnapshot(order)
+	return nil
+}
+
+func cloneOrderSnapshot(order models.OrderSnapshot) models.OrderSnapshot {
+	order.Parts = order.Parts.Clone()
+	return order
+}
