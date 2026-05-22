399e9628993eea5df1419b6fe1d6579db1dba3c2
Sergei Skoropupov <donckou@gmail.com>
2026-05-22 05:52:31 +0300

    refactor: убрал мусор из main.go

--- services/order/cmd/order/main.go
+++ services/order/cmd/order/main.go
@@ -4,38 +4,8 @@ import (
 	"context"
 	"log"
 
-	"github.com/google/uuid"
-
-	orders "github.com/BeInBloom/big-deal/generated/go/order-service/openapi/v1"
 	"github.com/BeInBloom/big-deal/services/order/internal/app"
 )
-
-type Handler struct {
-	orders.UnimplementedHandler
-}
-
-func (h Handler) OrderServiceCreateOrder(
-	ctx context.Context,
-	req *orders.CreateOrderRequest,
-) (orders.OrderServiceCreateOrderRes, error) {
-	return &orders.CreateOrderResponse{
-		OrderUUID:  uuid.New(),
-		TotalPrice: 1234.56,
-	}, nil
-}
-
-func (h Handler) OrderServiceGetOrder(
-	ctx context.Context,
-	params orders.OrderServiceGetOrderParams,
-) (orders.OrderServiceGetOrderRes, error) {
-	return &orders.Order{
-		OrderUUID:  params.OrderUUID,
-		UserUUID:   uuid.New(),
-		PartUuids:  []uuid.UUID{uuid.New()},
-		TotalPrice: 1234.56,
-		Status:     orders.OrderStatusPENDINGPAYMENT,
-	}, nil
-}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
